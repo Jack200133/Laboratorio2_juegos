5106b5731a8ab3c717a51c06cfaba7fac5324544
Jack <car20593@uvg.edu.gt>
2021-01-26 19:26:22 -0600

    Laboratorio Juegos 2 Terminado

---
+++ Assets/Scrips/LevelManeger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManeger : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public Object playerfab;
+    GameObject currentP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentP = Instantiate(playerfab, spawnPoint.transform.position, Quaternion.identity) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnPoint && playerfab && Input.GetKeyDown(KeyCode.Return))
+            if(currentP == null)
+                currentP =  Instantiate(playerfab, spawnPoint.transform.position, Quaternion.identity) as GameObject;
+    }
+}
---
+++ Assets/Scrips/Movimiento.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    Rigidbody rb;
+    public float force = 4.0f;
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.005f)
+           
+                rb.AddForce(0, force*1.5f, 0, ForceMode.Impulse);
+         
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb)
+        {
+            rb.AddForce(Input.GetAxis("Horizontal") * force,
+                               0,
+                               Input.GetAxis("Vertical") * force);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstaculo"))
+        {
+            if (score < 2)
+                Destroy(gameObject);
+            else
+                collision.collider.enabled = false;
+            
+        }
+           
+        
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+            score++;
+  
+        }
+    }
+}
